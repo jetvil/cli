a43ffbd5424fb8722a483b802108d7c22d5be1ed
m10rten <qmaartenenzo@gmail.com>
2022-10-18 23:12:36 +0200

    fix ts-node-script being showed instead node in index.d.ts

--- replace-script-notation.ts
+++ replace-script-notation.ts
@@ -4,8 +4,11 @@ import { readFileSync, writeFileSync } from "fs";
 try {
   const indexCLIFile = readFileSync("./dist/index.js", "utf8");
   const newFile = indexCLIFile.replace("ts-node-script", "node");
-  // write the new file to the same location
   writeFileSync("./dist/index.js", newFile);
+
+  const indexDeclarationFile = readFileSync("./dist/index.d.ts", "utf8");
+  const newDeclarationFile = indexDeclarationFile.replace("ts-node-script", "node");
+  writeFileSync("./dist/index..d.ts", newDeclarationFile);
   process.exit(0);
 } catch (error) {
   console.error(error);
